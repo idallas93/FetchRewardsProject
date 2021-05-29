e76cb29047dfef13a1277cf367c5c41ff8e9cc42
idallas93 <Dallas.Isaac1@gmail.com>
2021-05-29 16:22:02 -0400

    removed yarn

--- fetch-project/src/App.js
+++ fetch-project/src/App.js
@@ -1,52 +1,52 @@
-import './App.css';
 import { useState, useEffect } from "react";
-import Items from "./Items"
+import Items from "./Items";
 
 const url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
 
 function App() {
   const [data, setData] = useState([]);
+  //fetch data from url provided by assignment
   const fetchItems = async () => {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setData(data)
-      console.log(data.filter((name) => name.name))
-
-    } catch(error){
+      //set state to be json object grabbed by fetch
+      setData(data);
+    } catch (error) {
       console.log(error);
     }
   };
 
-  let filteredArray = data.filter((name) => name.name)
+  //filter data to exclude objects with blank names
+  let filteredArray = data.filter((name) => name.name);
 
-  let sortedArray =  filteredArray.sort((a , b) => {
-    if(a.listId > b.listId) return 1;
-    if(a.listId < b.listId) return -1;
-    if(parseInt(a.name.split(" ")[1]) > parseInt(b.name.split(" ")[1])) return 1;
-    if(parseInt(a.name.split(" ")[1]) < parseInt(b.name.split(" ")[1])) return -1;  }) 
+  // sort data by list id and name
+  let sortedArray = filteredArray.sort((a, b) => {
+    if (a.listId > b.listId) return 1;
+    if (a.listId < b.listId) return -1;
+    if (parseInt(a.name.split(" ")[1]) > parseInt(b.name.split(" ")[1]))
+      return 1;
+    if (parseInt(a.name.split(" ")[1]) < parseInt(b.name.split(" ")[1]))
+      return -1;
+  });
 
-  console.log("sorted array",sortedArray)
- 
+  // console.log("sorted array",sortedArray)
 
+  //use effect hook to run fetchItems function and grab the data needed for application
   useEffect(() => {
     fetchItems();
-  }, [])
+  }, []);
+
+  //JSX code
   return (
     <main>
-    <section id='container'>
-      <h3>
-        Filtered list sorted by listId and name.
-      </h3>
-      {sortedArray.map((item, index) => {
-        return (
-      <Items key={index} {...item} >
-
-     </Items>
-        )
-      })}
-     
-    </section>
+      <section id="container">
+        <h3>Filtered list sorted by listId and name.</h3>
+        {/* map the sorted/ filtered data and return each object as its own item */}
+        {sortedArray.map((item, index) => {
+          return <Items key={index} {...item}></Items>;
+        })}
+      </section>
     </main>
   );
 }
--- fetch-project/src/Items.js
+++ fetch-project/src/Items.js
@@ -1,28 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-
-const Items = ({
-    id, 
-    listId,
-    name
-}) => {
-
-    return (
-        <article className="person">
-            <div>
-            <h4>
-                ID: {id}
-            </h4>
-            <p>
-                ListID: {listId}
-            </p>
-            <p>
-                Name: {name}
-            </p>
-            </div>
-        </article>
-    )
-
-}
+const Items = ({ id, listId, name }) => {
+  return (
+    <article className="person">
+      <div>
+        <h4>ID: {id}</h4>
+        <p>ListID: {listId}</p>
+        <p>Name: {name}</p>
+      </div>
+    </article>
+  );
+};
 
 export default Items;
